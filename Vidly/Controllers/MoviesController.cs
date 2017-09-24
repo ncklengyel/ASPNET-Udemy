70614e44ff73ccb6feb6b55905fd960b9076f9df
Nick Lengyel <ncklengyel@gmail.com>
2017-09-24 03:09:19 -0400

    Exercise 2

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(IndexOutOfRangeException id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index()
+        {
+          var movies =  new List<Movie>
+          {
+            new Movie { Id = 1, Name = "Shrek" },
+            new Movie { Id = 2, Name = "Wall-e" }
+          };
+
+
+            return View(movies);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
